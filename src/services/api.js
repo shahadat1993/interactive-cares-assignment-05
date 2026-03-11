89b1b6d10e9f648662d9d099a48e12fc3d18da19
shahadat1993 <mdshahadat199131@gmail.com>
2026-03-11 07:29:15 +0600

    new updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5002"
+//   baseURL: "http://localhost:5002"
+baseURL: "https://contact-api.onrender.com"
 });
 
 export default api;
