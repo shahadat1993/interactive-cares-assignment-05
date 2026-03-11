93b1946d7a50b8832b65129d0c96f0b10dbae0e4
shahadat1993 <mdshahadat199131@gmail.com>
2026-03-11 07:34:03 +0600

    api.js updated

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,10 @@
+
 import axios from "axios";
 
 const api = axios.create({
 //   baseURL: "http://localhost:5002"
-baseURL: "https://contact-api.onrender.com"
+
+  baseURL: "https://interactive-cares-assignment-05.onrender.com",
 });
 
 export default api;
